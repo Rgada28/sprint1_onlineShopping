821e5e824fd13d6ded3331c0d4881d7f360b7b96
Nitin Gada <raj.nitin-gada@capgemini.com>
2022-10-20 19:45:50 +0530

    Added Junit dependency

--- src/test/java/com/sprint/onlineShopping/OnlineShoppingApplicationTests.java
+++ src/test/java/com/sprint/onlineShopping/OnlineShoppingApplicationTests.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
